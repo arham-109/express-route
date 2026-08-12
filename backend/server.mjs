e1f81a01f53f10e5d1a3bd8507a42d1a7389ff6d
arham-109 <arhamshafiq2007@gmail.com>
2026-08-12 20:50:19 +0500

    Configured Railway

--- backend/server.mjs
+++ backend/server.mjs
@@ -3,12 +3,11 @@ import { postRouter } from "./routes/index.mjs";
 import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 //It is called a body parser middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.get("/", (req, res) => {
   res.send({
     message: "Hello World",
