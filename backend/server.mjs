8a939b93ad39e09b50bddd24b2515d006e6b65ef
arham-109 <arhamshafiq2007@gmail.com>
2026-08-12 21:32:22 +0500

    cors origin changed

--- backend/server.mjs
+++ backend/server.mjs
@@ -7,7 +7,7 @@ const port = process.env.port || 3000;
 
 //It is called a body parser middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL || "https://express-route-todo.vercel.app/" }));
+app.use(cors({ origin: process.env.CLIENT_URL || "https://express-route-todo.vercel.app" }));
 app.get("/", (req, res) => {
   res.send({
     message: "Hello World",
